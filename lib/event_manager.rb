0352ccac4859954cebfa3b7d1198a88f3156255a
BantAlex <bantalex3@gmail.com>
2024-06-23 17:44:50 +0300

    Find peak reg hours

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,6 +1,7 @@
 require 'csv'
 require 'google/apis/civicinfo_v2'
 require 'erb'
+require 'time'
 
 def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5,"0")[0..4]
@@ -13,8 +14,10 @@ def clear_phone_numbers(number)
     return clean_num.sub(clean_num[0],"")
   end
 
-  return clean_num unless clean_num.length < 10 || clean_num.length >= 11
-  print "Not a valid number."
+  if clean_num.length < 10 || clean_num.length >= 11
+    "Not a valid number."
+  end
+  clean_num
 end
 
 def legislators_by_zipcode(zip)
@@ -42,6 +45,12 @@ def save_thank_you_letter(id,form_letter)
   end
 end
 
+def format_date(date)
+  formatted_date = Time.strptime(date,"%m/%d/%y %H:%M")
+  formatted_date
+end
+
+
 
 puts 'EventManager initialized.'
 
@@ -54,14 +63,21 @@ contents = CSV.open(
 template_letter = File.read('form_letter.erb')
 erb_template = ERB.new template_letter
 
+time_array = []
+
 contents.each do |row|
   id = row[0]
   name = row[:first_name]
+  reg_date = row[:regdate]
+  time_array.push(format_date(reg_date).hour)
+
   zipcode = clean_zipcode(row[:zipcode])
   legislators = legislators_by_zipcode(zipcode)
   phone_numbers = clear_phone_numbers(row[:homephone])
   form_letter = erb_template.result(binding)
 
-  puts phone_numbers
   # save_thank_you_letter(id,form_letter)
 end
+
+peak_reg_hours = time_array.sum / time_array.length
+puts "The average registration time is #{peak_reg_hours.to_i}:00"
